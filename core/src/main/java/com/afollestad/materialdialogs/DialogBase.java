bfa76835c97ff3215bbc7d11ff82ad6e59185f79
Ido Fang Bentov <dodobentov@gmail.com>
2019-02-10 16:43:35 +0200

    Update libraries and migrate to AndroidX

--- core/src/main/java/com/afollestad/materialdialogs/DialogBase.java
+++ core/src/main/java/com/afollestad/materialdialogs/DialogBase.java
@@ -3,65 +3,64 @@ package com.afollestad.materialdialogs;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.afollestad.materialdialogs.internal.MDRootLayout;
 
-/** @author Aidan Follestad (afollestad) */
+import androidx.annotation.Nullable;
+
+/**
+ * @author Aidan Follestad (afollestad)
+ */
 class DialogBase extends Dialog implements DialogInterface.OnShowListener {
 
-  protected MDRootLayout view;
-  private OnShowListener showListener;
+    protected MDRootLayout view;
+    private OnShowListener showListener;
 
-  DialogBase(Context context, int theme) {
-    super(context, theme);
-  }
-
-  @Override
-  public View findViewById(int id) {
-    return view.findViewById(id);
-  }
-
-  @Override
-  public final void setOnShowListener(@Nullable OnShowListener listener) {
-    showListener = listener;
-  }
-
-  final void setOnShowListenerInternal() {
-    super.setOnShowListener(this);
-  }
-
-  final void setViewInternal(View view) {
-    super.setContentView(view);
-  }
-
-  @Override
-  public void onShow(DialogInterface dialog) {
-    if (showListener != null) {
-      showListener.onShow(dialog);
+    DialogBase(Context context, int theme) {
+        super(context, theme);
     }
-  }
 
-  @Override
-  @Deprecated
-  public void setContentView(int layoutResID) throws IllegalAccessError {
-    throw new IllegalAccessError(
-        "setContentView() is not supported in MaterialDialog. Specify a custom view in the Builder instead.");
-  }
+    @Override
+    public final void setOnShowListener(@Nullable OnShowListener listener) {
+        showListener = listener;
+    }
 
-  @Override
-  @Deprecated
-  public void setContentView(View view) throws IllegalAccessError {
-    throw new IllegalAccessError(
-        "setContentView() is not supported in MaterialDialog. Specify a custom view in the Builder instead.");
-  }
+    final void setOnShowListenerInternal() {
+        super.setOnShowListener(this);
+    }
 
-  @Override
-  @Deprecated
-  public void setContentView(View view, @Nullable ViewGroup.LayoutParams params)
-      throws IllegalAccessError {
-    throw new IllegalAccessError(
-        "setContentView() is not supported in MaterialDialog. Specify a custom view in the Builder instead.");
-  }
+    final void setViewInternal(View view) {
+        super.setContentView(view);
+    }
+
+    @Override
+    public void onShow(DialogInterface dialog) {
+        if (showListener != null) {
+            showListener.onShow(dialog);
+        }
+    }
+
+    @Override
+    @Deprecated
+    public void setContentView(int layoutResID) throws IllegalAccessError {
+        throw new IllegalAccessError(
+                "setContentView() is not supported in MaterialDialog. Specify a custom view in the Builder instead.");
+    }
+
+    @Override
+    @Deprecated
+    public void setContentView(View view) throws IllegalAccessError {
+        throw new IllegalAccessError(
+                "setContentView() is not supported in MaterialDialog. Specify a custom view in the Builder instead.");
+    }
+
+    @Override
+    @Deprecated
+    public void setContentView(View view, @Nullable ViewGroup.LayoutParams params)
+            throws IllegalAccessError {
+        throw new IllegalAccessError(
+                "setContentView() is not supported in MaterialDialog. Specify a custom view in the Builder instead.");
+    }
 }
